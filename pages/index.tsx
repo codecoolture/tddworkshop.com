d07e456e75f53c420161f7ab1c85b8081cda5c71
Sergio Álvarez <sergio@codecoolture.com>
2021-12-04 21:38:17 +0100

    Add subheading for contact form

--- pages/index.tsx
+++ pages/index.tsx
@@ -152,6 +152,8 @@ export default function Homepage() {
             También puedes escribirme un email a <strong>hola@codecoolture.com</strong>.
           </Page.Text>
 
+          <Page.Subheading>¿Hablamos?</Page.Subheading>
+
           <Page.ContactForm onSuccessMessage="¡Gracias! Mensaje recibido.">
             <Page.ContactForm.Field name="email">
               <Page.ContactForm.Field.Label>Tu email</Page.ContactForm.Field.Label>
