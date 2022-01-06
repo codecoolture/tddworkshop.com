2d773710ceaf25054182fd196520a33dc71acb5b
Sergio Álvarez <sergio@codecoolture.com>
2022-01-06 13:51:03 +0100

    Make Jest to ignore Cypress tests

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const jest = require("next/jest");
 
-module.exports = jest()({});
+module.exports = jest()({
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/cypress/"],
+});
